f18ecdb27c158cc31abfb304a41dec7fdf268dcc
jadsongmatos <jadson.g-matos@outlook.com>
2023-07-26 19:57:22 -0300

    Error: Channel ended, no reply will be forthcoming

--- main.ts
+++ main.ts
@@ -2,12 +2,14 @@ import amqp from 'amqplib';
 import axios from "axios";
 import { PrismaClient } from "@prisma/client";
 
+const CONSUMER_COUNT = 10;
 const MAX_CEP = 99999999;
 const API_URL = "https://brasilapi.com.br/api/cep/v2/";
 const RABBITMQ_URL = "amqp://localhost";
 
 let channel: any;
 let prisma = new PrismaClient();
+let connection: any;
 
 // Function to read the ceps.bin file and get the biggest zip code
 async function getStartingZip() {
@@ -52,7 +54,7 @@ async function addToSearchQueue() {
         console.log("CEP", cep)
       }
       cep++;
-      setImmediate(addNextCepToQueue); // Call the next CEP in the next iteration of the event loop
+      addNextCepToQueue()
     } else {
       channel.sendToQueue('searchQueue', Buffer.from(JSON.stringify({ end: true })));
     }
@@ -63,8 +65,6 @@ async function addToSearchQueue() {
 }
 
 async function consumeSearchQueue() {
-  const CONSUMER_COUNT = 10;
-
   for (let i = 0; i < CONSUMER_COUNT; i++) {
     channel.consume('searchQueue', async (message: any) => {
       const data = JSON.parse(message.content.toString());
@@ -85,50 +85,91 @@ async function consumeSearchQueue() {
 }
 
 async function consumeWriteQueue() {
-  const CONSUMER_COUNT = 10;
   let batch: Array<any> = []
+  channel.consume('writeQueue', async (message: any) => {
+    const data = JSON.parse(message.content.toString());
+    if (data.end) {
+      try {
+        await prisma.$transaction(batch);
+        console.log("Final batch written")
+      } catch (error: any) {
+        console.log('prisma error', error)
+      }
+      batch = [];
+    } else {
+      const cep: number = data;
+      console.log("W", cep)
+      batch.push(prisma.ceps.create({ data: { cep: cep } }));
 
-  for (let i = 0; i < CONSUMER_COUNT; i++) {
-    channel.consume('writeQueue', async (message: any) => {
-      const data = JSON.parse(message.content.toString());
-      if (data.end) {
+      if (batch.length >= 128) {
         try {
           await prisma.$transaction(batch);
-          console.log("Final batch written")
+          console.log("Batch written")
         } catch (error: any) {
           console.log('prisma error', error)
         }
         batch = [];
-      } else {
-        const cep: number = data;
-        console.log("W", cep)
-        batch.push(prisma.ceps.create({ data: { cep: cep } }));
-
-        if (batch.length >= 128) {
-          try {
-            await prisma.$transaction(batch);
-            console.log("Batch written")
-          } catch (error: any) {
-            console.log('prisma error', error)
-          }
-          batch = [];
-        }
       }
-      channel.ack(message);
+    }
+    channel.ack(message);
+  });
+
+}
+
+async function createChannel() {
+  try {
+    channel = await connection.createChannel();
+
+    channel.on('close', () => {
+      console.error('RabbitMQ channel closed, recreating...');
+      setTimeout(createChannel, 1000);
     });
+
+    channel.on('error', (error: any) => {
+      if (error.message !== "Channel closing") {
+        console.error("[AMQP] channel error", error.message);
+      }
+    });
+
+    await channel.assertQueue('searchQueue');
+    await channel.assertQueue('writeQueue');
+
+    // Start consuming and adding to the queue in parallel
+    await Promise.all([addToSearchQueue(), consumeSearchQueue(), consumeWriteQueue()]);
+
+  } catch (error) {
+    console.error('Failed to create a RabbitMQ channel, retrying...', error);
+    setTimeout(createChannel, 1000);
   }
 }
 
-// Main function to start the script
-async function main() {
-  const connection = await amqp.connect(RABBITMQ_URL);
-  channel = await connection.createChannel();
+async function connectRabbitMQ() {
+  try {
+    connection = await amqp.connect(RABBITMQ_URL, {
+      timeout: 5000,
+    });
+    connection.on('close', () => {
+      console.error('RabbitMQ connection closed, reconnecting...');
+      setTimeout(connectRabbitMQ, 1000);
+    });
 
-  await channel.assertQueue('searchQueue');
-  await channel.assertQueue('writeQueue');
+    connection.on('error', (error: any) => {
+      if (error.message !== "Connection closing") {
+        console.error("[AMQP] conn error", error.message);
+      }
+    });
 
-  // Start consuming and adding to the queue in parallel
-  await Promise.all([addToSearchQueue(), consumeSearchQueue(), consumeWriteQueue()]);
+    connection.on("close", function () {
+      console.error("[AMQP] reconnecting");
+      return setTimeout(connectRabbitMQ, 1000);
+    });
+
+    createChannel();
+
+  } catch (error) {
+    console.error('Failed to connect to RabbitMQ, retrying...', error);
+    setTimeout(connectRabbitMQ, 1000);
+  }
 }
 
-main().catch(console.error);
+connectRabbitMQ()
